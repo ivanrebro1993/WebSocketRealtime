4358ffa6e95322cd825a0a82df941e61361b8468
Rebro Ivan <ivanrebro1993@gmail.com>
2018-01-28 23:08:59 +0700

    Add LineChart component

--- app/src/frontend/App.js
+++ app/src/frontend/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Line } from 'react-chartjs-2';
+import LineChart from './components/LineChart';
 import MetricsApi from './api/MetricsApi';
 const config = require('../etc/config.json');
 
@@ -43,32 +43,7 @@ class App extends React.Component {
     render() {
         return (
             <div class='App'>
-                <Line data={{
-                   labels: this.state.labels,
-                       datasets: [
-                           {
-                               label: `Metrics values from server`,
-                               fill: true,
-                               lineTension: 0.1,
-                               backgroundColor: 'rgba(75,192,192,0.4)',
-                               borderColor: 'rgba(75,192,192,1)',
-                               borderCapStyle: 'butt',
-                               borderDash: [],
-                               borderDashOffset: 0.0,
-                               borderJoinStyle: 'miter',
-                               pointBorderColor: 'rgba(75,192,192,1)',
-                               pointBackgroundColor: '#fff',
-                               pointBorderWidth: 1,
-                               pointHoverRadius: 5,
-                               pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                               pointHoverBorderColor: 'rgba(220,220,220,1)',
-                               pointHoverBorderWidth: 2,
-                               pointRadius: 1,
-                               pointHitRadius: 10,
-                               data: this.state.data
-                           }
-                     ]
-                   }} redraw />
+                <LineChart data={this.state.data} labels={this.state.labels} />
             </div>
         );
     }
